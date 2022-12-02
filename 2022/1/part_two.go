894d22b9bf702a265fdbe11e48b20ddc8780177f
Dylan Meskis <dmeskis@gmail.com>
2022-12-01 20:52:49 -0700

    Day 1 complete

--- 2022/1/part_two.go
+++ 2022/1/part_two.go
@@ -8,30 +8,40 @@ import (
 	"strconv"
 )
 
+// sorting
+
 func main() {
 	fi, err := os.Stdin.Stat()
 	if err != nil {
 		panic(err)
 	}
 
+	top3 := []int{0, 0, 0}
+
 	if fi.Size() > 0 {
 		s := bufio.NewScanner(os.Stdin)
 		sum := 0
-		total := 0
 		for s.Scan() {
 			if s.Text() == "" {
-				if sum > total {
-					total = sum
+				for i, val := range top3 {
+					if sum > val {
+						top3[i] = sum
+						break
+					}
 				}
 				sum = 0
 			} else {
-
 				val, err := strconv.Atoi(s.Text())
 				if err != nil {
 					panic(err)
 				}
 				sum += val
 			}
+		}
+
+		total := 0
+		for _, val := range top3 {
+			total += val
 		}
 		fmt.Println(total)
 	} else {
